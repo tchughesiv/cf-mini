4c1865c578f8f0706c4a30aa752fc1a0245dabcd
tchughesiv <tchughesiv@gmail.com>
2015-05-15 17:02:04 -0500

    update

--- insert.sh
+++ insert.sh
@@ -2,12 +2,12 @@
 file1=/var/vcap/data/packages/dea_next/b2fac8dac45fe1796cc982860b8549bbe78ca55f/vendor/cache/warden-ad18bff7dc56/warden/root/linux/net.sh
 file2=/var/vcap/data/packages/dea_next/b2fac8dac45fe1796cc982860b8549bbe78ca55f/vendor/cache/warden-ad18bff7dc56/warden/root/linux/skeleton/net.sh
 file3=/var/vcap/data/packages/warden/724f030f2d6e90d02c2afbe90ed5fe1ce2de1667/warden/root/linux/net.sh
-file4=/var/vcap/data/packages/warden/724f030f2d6e90d02c2afbe90ed5fe1ce2de1667/warden/root/linux/skeleton/net.sh
+# file4=/var/vcap/data/packages/warden/724f030f2d6e90d02c2afbe90ed5fe1ce2de1667/warden/root/linux/skeleton/net.sh
 
 cp -p $file1 $file1.bak
 cp -p $file2 $file2.bak
 cp -p $file3 $file3.bak
-cp -p $file4 $file4.bak
+# cp -p $file4 $file4.bak
 
 while read line
 do
@@ -36,18 +36,19 @@ iptables -t nat -A warden-prerouting -d 0.0.0.0/32 -j DNAT --to-destination $NIS
 iptables -t nat -A warden-postrouting -s $NISE_IP_ADDRESS/32 -j SNAT --to-source 0.0.0.0'
 done < $file3 > $file3.new
 
-while read line
-do
-        echo $line
-        echo $line | grep -q "echo 1 > /proc/sys/net/ipv4/ip_forward"
-        [ $? -eq 0 ] && echo 'NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d ' ' | cut -f1 -d '/' | head -n 1`}
-iptables -t nat -A warden-prerouting -d 0.0.0.0/32 -j DNAT --to-destination $NISE_IP_ADDRESS
-iptables -t nat -A warden-postrouting -s $NISE_IP_ADDRESS/32 -j SNAT --to-source 0.0.0.0'
-done < $file3 > $file3.new
+# while read line
+# do
+#         echo $line
+#         echo $line | grep -q "echo 1 > /proc/sys/net/ipv4/ip_forward"
+#         [ $? -eq 0 ] && echo 'NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d ' ' | cut -f1 -d '/' | head -n 1`}
+# iptables -t nat -A warden-prerouting -d 0.0.0.0/32 -j DNAT --to-destination $NISE_IP_ADDRESS
+# iptables -t nat -A warden-postrouting -s $NISE_IP_ADDRESS/32 -j SNAT --to-source 0.0.0.0'
+# done < $file3 > $file3.new
 
-chmod +x $file1.new $file2.new $file3.new $file4.new
+chmod +x $file1.new $file2.new $file3.new
+# $file4.new
 
 mv $file1.new $file1
 mv $file2.new $file2
 mv $file3.new $file3
-mv $file4.new $file4
+# mv $file4.new $file4
