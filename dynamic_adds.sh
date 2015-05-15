d6da2328f226cc5f85be7c4dd26eb3c657992eed
tchughesiv <tchughesiv@gmail.com>
2015-05-15 00:52:07 -0500

    update

--- dynamic_adds.sh
+++ dynamic_adds.sh
@@ -5,5 +5,5 @@
 sed -i 's/buildpack_java/buildpack_java_offline/g' /root/cf_nise_installer/manifests/template.yml
 sed -i 's/^    hm9000_noop: false/    hm9000_noop: false\n    default_app_memory: 512\n    default_app_disk_in_mb: 384/g' /root/cf_nise_installer/manifests/template.yml
 
-echo "\naddress=/$NISE_DOMAIN/0.0.0.0" >> /etc/dnsmasq.conf
-echo 'sed -i "s/${NISE_IP_ADDRESS}/0.0.0.0/g" manifests/deploy.yml' >> /root/cf_nise_installer/scripts/generate_deploy_manifest.sh
+echo "\naddress=/$NISE_DOMAIN/127.0.0.1" >> /etc/dnsmasq.conf
+echo 'sed -i "s/${NISE_IP_ADDRESS}/127.0.0.1/g" manifests/deploy.yml' >> /root/cf_nise_installer/scripts/generate_deploy_manifest.sh
